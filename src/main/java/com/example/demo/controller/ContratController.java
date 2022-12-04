4ea973cd07416ede0696571560e1e02e5250c4a4
Mahdi El Ghali <60888933+mahdielghali@users.noreply.github.com>
2022-12-04 18:45:57 +0100

    to connect to front

--- src/main/java/com/example/demo/controller/ContratController.java
+++ src/main/java/com/example/demo/controller/ContratController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Date;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @AllArgsConstructor
 @RestController
 public class ContratController {
